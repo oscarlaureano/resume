248cf44f6524a6cc7228a1f2215f49278e8679c1
Oscar Laureano <oscar.laureano.xx@gmail.com>
2021-07-04 09:26:53 -0500

    Added: Changing text. Modified: Position of toggle switch

--- script.js
+++ script.js
@@ -1,3 +1,47 @@
+
+var timeIntervalNormalText = 1000;
+var timeIntervalGlitchyOverlap = 3000;
+var titlesList = [
+    "B.S. COMPUTER SCIENCE",
+    "ROCKSTAR",
+    "SOFTWARE ENGINEER",
+    "SENIOR DEVELOPER",
+    "IoT ENTHUSIAST",
+    "DATA SCIENCE FAN",
+    "PYTHON APPRENTICE",
+    "JAVASCRIPT ADHERENT",
+    "SCRUM MASTER",
+    "GONE FERAL."
+];
+
+function switchTitles(){
+    var changingText = setInterval(changeTitle, timeIntervalNormalText);
+    var changingOverlap = setInterval(changeOverlap, timeIntervalGlitchyOverlap); 
+    
+}
+
+function changeTitle(){
+    document.getElementById("magicTitle").innerHTML = getTitlefromList();
+}
+
+function changeOverlap(){
+    var randomIndex = Math.floor((Math.random() * titlesList.length));
+    var glitchyText = document.getElementById("magicTitle");
+    glitchyText.setAttribute("data-text", titlesList[randomIndex]);
+}
+
+function getTitlefromList(){
+    var actualTitle = document.getElementById("magicTitle").value;
+    var randomIndex = Math.floor((Math.random() * titlesList.length));
+    timeIntervalNormalText = Math.random() + 1;
+    console.log(actualTitle);
+    while(titlesList[randomIndex] === actualTitle){
+        randomIndex = Math.floor((Math.random() * titlesList.length));
+    }
+
+    return titlesList[randomIndex];
+}
+
 window.addEventListener('DOMContentLoaded', (event) => {
 
     const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
